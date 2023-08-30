38b11bb0ccdfe6d79fb77a524a1291a1403e9e2e
1mukl1 <misha8090@gmail.com>
2023-08-30 21:55:36 +0300

    Добавлена возможность считывания n-го количества строк и полной записи в файл результатов выражения

--- src/Main.java
+++ src/Main.java
@@ -4,46 +4,53 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        try{
-            File file = new File("Путь к файлу для чтения");
-            Scanner sc = new Scanner(file);
-            File result = new File("Путь к файлу для запсиси результата");
-            FileWriter writer = new FileWriter(result);
-            String str = sc.nextLine();
-            double res = 0;
-            String[] str1 = str.split(" ");
-            double a = Double.parseDouble(str1[0]);
-            double b = Double.parseDouble(str1[2]);
-            if (str1[1].equals("/") && str1[2].equals("0")){
-                throw new Exception("Error! Division by zero");
+        File file = new File("C://Users//misha//OneDrive//Рабочий стол//input.txt");
+        Scanner sc = new Scanner(file);
+        File result = new File("C://Users//misha//OneDrive//Рабочий стол//output.txt");
+        FileWriter writer = new FileWriter(result);
+        String str = "";
+        int counter = 0;
+        double res = 0;
+        while (sc.hasNext()) {
+            try {
+                str = sc.nextLine();
+                String[] str1 = str.split(" ");
+                double a = Double.parseDouble(str1[0]);
+                double b = Double.parseDouble(str1[2]);
+                if (str1[1].equals("/") && str1[2].equals("0")) {
+                    writer.write(str + " = Error! Division by zero" + "\n");
+                    throw new Exception("Error! Division by zero");
+                }
+                if (!str1[1].equals("+") && !str1[1].equals("-") && !str1[1].equals("*") && !str1[1].equals("/")) {
+                    writer.write(str + " = Operation Error!" + "\n");
+                    throw new Exception("Operation Error!");
+                } else {
+                    String op = str1[1];
+                    if (op.equals("+")) {
+                        res = a + b;
+                    }
+                    if (op.equals("-")) {
+                        res = a - b;
+                    }
+                    if (op.equals("*")) {
+                        res = a * b;
+                    }
+                    if (op.equals("/")) {
+                        res = a / b;
+                    }
+                }
+                System.out.println(res);
+                writer.write(str + " = " + String.valueOf(res) + "\n");
             }
-            if (!str1[1].equals("+") && !str1[1].equals("-") && !str1[1].equals("*") && !str1[1].equals("/")){
-                throw new Exception("Operation Error!");
-            } else {
-                String op = str1[1];
-                if (op.equals("+")){
-                    res = a + b;
-                }
-                if (op.equals("-")){
-                    res = a - b;
-                }
-                if (op.equals("*")){
-                    res = a * b;
-                }
-                if (op.equals("/")){
-                    res = a / b;
-                }
+        catch(NumberFormatException nf){
+                System.out.println("Error! Not number");
+                writer.write(str + " = Error! Not number" + "\n");
             }
-            System.out.println(res);
-            writer.write(String.valueOf(res));
-            sc.close();
-            writer.close();
+        catch(Exception ex){
+                System.out.println(ex.getMessage());
+            }
         }
-        catch (NumberFormatException nf){
-            System.out.println("Error! Not number");
-        }
-        catch (Exception ex){
-            System.out.println(ex.getMessage());
-        }
+        sc.close();
+        writer.close();
     }
 }
